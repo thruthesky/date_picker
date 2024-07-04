4db9380d89a7b85fdb401d469d6e72662c9b2646
JaeHo Song <thruthesky@gmail.com>
2024-07-04 11:11:17 +0800

    update date picker

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.0.2
+
+* First version of date picker
+
 ## 0.0.1
 
-* TODO: Describe initial release.
+* Initial scaffold
--- lib/date_picker.dart
+++ lib/date_picker.dart
@@ -1,7 +1,1 @@
-library date_picker;
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export './src/date_picker.dart';
