f55b27ada512554687ad32cd0dd8d91441a515e9
JaeHo Song <thruthesky@gmail.com>
2024-07-04 10:35:41 +0800

    adding basic scaffold

--- test/date_picker_test.dart
+++ test/date_picker_test.dart
@@ -1,6 +1,5 @@
+import 'package:date_picker_v2/date_picker.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:date_picker/date_picker.dart';
 
 void main() {
   test('adds one to input values', () {
